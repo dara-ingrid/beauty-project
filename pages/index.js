b95d610f7cba1e1f15c3ab6be4162b5f50a52a64
Dara Ferreira <dara-ingrid@hotmail.com>
2024-09-02 19:41:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi, Babyinho! Se você me ama, dê uma risadinha!</h1>
+    return <h1>Oi, Babyinho! Se você me ama, dê uma risadinha! 😎💞 </h1>
 }
 
 export default Home;
